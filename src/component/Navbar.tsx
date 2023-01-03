95a3d7522d8427213b5c2deae7c36c59ab59c40a
kenneth vega <kenneth.trinidad.vega@gmail.com>
2023-01-03 09:20:15 +0800

    initial setup

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from './utility/container';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Container>Navbar</Container>
+    </div>
+  );
+};
+
+export default Navbar;
